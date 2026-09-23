a72ce5efccf82a18b8d0a09deb251c5f16c838b4
rupesh1820 <your-email@example.com>
2026-09-23 12:09:43 +0530

    ho gya

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -73,7 +73,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-
+{/* DOne */}
       <Footer />
     </div>
   );
